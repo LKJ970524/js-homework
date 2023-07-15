170924700accdbf0148c2644ccd9c1d108ba7884
LKJ970524 <dlrbwjd7096@naver.com>
2023-07-15 16:13:47 +0900

    이메일 비밀번호 함수 생성

--- mission01/js/main.js
+++ mission01/js/main.js
@@ -4,6 +4,11 @@ const user = {
   pw:'spdlqj123!@'
 }
 
+
+
+
+
+
 /*
 
 1. email 정규표현식을 사용한 validation
@@ -13,24 +18,44 @@ const user = {
 
 */
 
-function emailReg(text){
+function emailReg(text) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return re.test(String(text).toLowerCase())
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
 
+const idInput = document.getElementById('userEmail')
+const pwInput = document.getElementById('userPassword')
+const loginB = document.querySelector('.btn-login')
 
 
 
 
+function validateId(text) {
+  const isIdValid = emailReg(text);
+  if (!isIdValid) {
+    console.log('이메일 형식이 올바르지 않습니다.');
+    idInput.classList.add('is--invalid');
+  } else {
+    idInput.classList.remove('is--invalid');
+  }
+  return isIdValid;
+}
 
-
-
+function validatePw(text) {
+  const isPwValid = pwReg(text);
+  if (!isPwValid) {
+    console.log('비밀번호 형식이 올바르지 않습니다.');
+    pwInput.classList.add('is--invalid');
+  } else {
+    pwInput.classList.remove('is--invalid');
+  }
+  return isPwValid;
+}
 
 
